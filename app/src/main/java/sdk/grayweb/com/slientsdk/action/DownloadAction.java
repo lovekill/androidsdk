b8553e02ddd55dcde49c3382acde75c403214758
love_kill <lihongbo2011@gmail.com>
2016-05-18 21:21:09 +0800

    ee

--- app/src/main/java/sdk/grayweb/com/slientsdk/action/DownloadAction.java
+++ app/src/main/java/sdk/grayweb/com/slientsdk/action/DownloadAction.java
@@ -15,53 +15,52 @@ import sdk.grayweb.com.slientsdk.util.DownLoadUtil;
 /**
  * Created by engine on 16/5/11.
  */
-public class DownloadAction extends AbstractAction{
+public class DownloadAction extends AbstractAction {
+    private FileDownloader mFileDownloader;
+
     public DownloadAction(Context context) {
         super(context);
+        mFileDownloader = new FileDownloader(1);
     }
 
     @Override
     public int doAction() {
         try {
-            List<ApkModel> downloadApks =  apkModelDao.queryBuilder().orderBy("level",false).query();
-            for (ApkModel model:downloadApks){
-                if(!DownLoadUtil.hasDown(model.apkpath)) {
+            List<ApkModel> downloadApks = apkModelDao.queryBuilder().orderBy("level", false).query();
+            for (ApkModel model : downloadApks) {
+                if (!DownLoadUtil.hasDown(model.apkpath)) {
                     doDown(model.apkpath);
                     break;
                 }
             }
-            return  downloadApks.size();
+            return downloadApks.size();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return  -1 ;
+        return -1;
     }
-    private void doDown(String url){
-        FileDownloader mFileDownloader;
-// Create file downloader
-        mFileDownloader = new FileDownloader(1);
 
-// Create file downloader listener
+    private void doDown(String url) {
         FileDownloader.Listener listener = mFileDownloader.new Listener() {
             @Override
             public void onStart() {
                 // download start
-                Log.e(TAG,"download start");
+                Log.e(TAG, "download start");
             }
 
             public void onFinish() {
                 // download finish
-                Log.e(TAG,"download finish");
+                Log.e(TAG, "download finish");
             }
 
             public void onError(String msg) {
                 // download error
-                Log.e(TAG,"download error");
+                Log.e(TAG, "download error");
             }
 
             public void onProgressChange(long fileSize, long downloadedSize) {
                 // download progress change
-                Log.e(TAG,"download progress fileSize="+fileSize+",downloadedSize="+downloadedSize);
+                Log.e(TAG, "download progress fileSize=" + fileSize + ",downloadedSize=" + downloadedSize);
             }
         };
         mFileDownloader.add(DownLoadUtil.getFileNameByUrl(url), url, listener);
